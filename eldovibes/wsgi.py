efb7958f87833a9e55a63b7da303653422e7a317
ron-kiplimo <rkkiplimo@gmail.com>
2025-05-11 15:09:52 +0300

    Renamed eldorethot to eldovibes in wsgi.py

--- eldovibes/wsgi.py
+++ eldovibes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eldorethot project.
+WSGI config for eldovibes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eldorethot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eldovibes.settings')
 
 application = get_wsgi_application()
